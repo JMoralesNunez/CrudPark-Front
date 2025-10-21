b09545dfea35fe42a5253c151b1daf257254bed6
JhonatanMrls <jhonatanmorales99@gmail.com>
2025-10-20 20:56:35 -0500

    api url updated

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://parkingcrud.onrender.com/api'; 
+const API_BASE_URL = 'https://parkingcrud2-0.onrender.com/api'; 
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
